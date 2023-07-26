12ac8e553e0497e6fc41c2e1192fc40e315cc63e
yungsamd17 <64147848+yungsamd17@users.noreply.github.com>
2023-07-26 22:06:21 +0200

    ⬆️Added Open in Spotify Desktop client

---
+++ scripts/Open-in-Spotify-Desktop-client.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Open in Spotify Desktop client
+// @description  Opens spotify links in the desktop app
+// @version      0.1
+// @author       yungsamd17
+// @namespace    http://tampermonkey.net/
+// @license      MIT License
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=spotify.com
+// @match        https://open.spotify.com/*
+// @run-at       document-start
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var data=document.URL.match(/[\/\&](track|playlist|album|artist|show|episode)\/([^\&\#\/\?]+)/i);
+    console.log("This is a "+data[1]+" with id:"+data[2]+"\nAttempting to redirect");
+    window.location.replace('spotify:'+data[1]+':'+data[2]);
+
+    setTimeout(function() {
+      console.log("Closing tab");
+      window.close();
+    }, 500);
+})();

fb10695cc081b0159c12c288db791dcfd576eacf
yungsamd17 <64147848+yungsamd17@users.noreply.github.com>
2023-08-03 13:53:22 +0200

    :wrench: Create My-Userscript-Template.user.js

---
+++ My-Userscript-Template.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New script {{name}}
+// @description  {{date}}
+// @version      1.0
+// @author       yungsamd17
+// @namespace    https://github.com/yungsamd17/UserScripts
+// @license      MIT License
+// @downloadURL  https://github.com/yungsamd17/UserScripts/raw/main/scripts/[USERSCRIPT_NAME].user.js
+// @updateURL    https://github.com/yungsamd17/UserScripts/raw/main/scripts/[USERSCRIPT_NAME].user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=[URL]
+// @match        {{url}}
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==

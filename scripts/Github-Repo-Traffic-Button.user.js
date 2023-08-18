337a98643c03559e9911cfd9425c7078df14fcaa
yungsamd17 <64147848+yungsamd17@users.noreply.github.com>
2023-08-19 00:12:54 +0200

    :wrench: Create Github-Repo-Traffic-Button.user.js

---
+++ scripts/Github-Repo-Traffic-Button.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         GitHub Repo Traffic Button
+// @description  Add a button to view current repo traffic on GitHub
+// @version      1.0
+// @namespace    https://github.com/yungsamd17/UserScripts
+// @license      MIT License
+// @downloadURL  https://github.com/yungsamd17/UserScripts/raw/main/scripts/Github-Repo-Traffic-Button.user.js
+// @updateURL    https://github.com/yungsamd17/UserScripts/raw/main/scripts/Github-Repo-Traffic-Button.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @match        https://github.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addRepoTrafficButton() {
+        const sidebar = document.querySelector('.Layout-sidebar');
+        if (sidebar) {
+            const repoUrl = window.location.href;
+            const username = repoUrl.split('/')[3];
+            const repoName = repoUrl.split('/')[4];
+
+            const trafficButton = document.createElement('a');
+            trafficButton.href = `https://github.com/${username}/${repoName}/graphs/traffic`;
+            trafficButton.className = 'btn btn-sm mb-2 d-block mx-auto text-center';
+            trafficButton.textContent = 'Repo Traffic';
+            trafficButton.style.margin = '10px auto';
+
+            sidebar.appendChild(trafficButton);
+        }
+    }
+
+    addRepoTrafficButton();
+})();

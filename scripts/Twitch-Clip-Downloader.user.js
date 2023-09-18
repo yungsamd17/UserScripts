ed52284d5dd41d241c2ab67820a26fcc3a74df6d
yungsamd17 <yungsamd@proton.me>
2023-09-18 07:35:09 +0200

    🔧Fixed channel switch and button issues ...
    ... Bug fixes: Resolved button issues when switching channels and navigating from main site to streams or clips, which also fixed a related problem with the `Download` button not being removed on non clip pages.

--- scripts/Twitch-Clip-Downloader.user.js
+++ scripts/Twitch-Clip-Downloader.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Twitch Clip Downloader
 // @description  Quick and easy way to download Twitch Clips.
-// @version      1.0
+// @version      1.1
 // @author       yungsamd17
 // @namespace    https://github.com/yungsamd17/UserScripts
 // @license      MIT License
 // @downloadURL  https://github.com/yungsamd17/UserScripts/raw/main/scripts/Twitch-Clip-Downloader.user.js
 // @updateURL    https://github.com/yungsamd17/UserScripts/raw/main/scripts/Twitch-Clip-Downloader.user.js
 // @icon         https://raw.githubusercontent.com/yungsamd17/UserScripts/main/scripts/icons/Twitch-Clip-Downloader.png
+// @match        https://www.twitch.tv/*
 // @match        https://clips.twitch.tv/*
 // @grant        GM_download
 // @run-at       document-end
@@ -16,72 +17,100 @@
 (function() {
     'use strict';
 
-    // download twitch clip
+    // create the button
+    function createButton() {
+        const targetClass = '[class*="Layout-sc-1xcs6mc-0"][class*="player-controls__right-control-group"]';
+        const targetElement = document.querySelector(targetClass);
+        if (!targetElement) {
+            // if target element is not found, remove any existing button
+            removeButton();
+            return;
+        }
+        // check if we are on a clip page
+        const isClipPage = window.location.href.includes('/clip/') || window.location.href.includes('clips.twitch.tv');
+        if (!isClipPage) {
+            removeButton();
+            return;
+        }
+        // check if the button already exists
+        const existingButton = document.querySelector('.twitchdownload-button');
+        if (existingButton) {
+            return;
+        }
+
+        // button CSS style
+        const customStyles = `
+            .twitchdownload-button {
+                background-color: #9147ff;
+                border: none;
+                border-radius: 0.4rem;
+                margin-right: 6px;
+                padding: 5px 2px 5px 2px;
+                cursor: pointer;
+                width: auto;
+                height: 30px;
+                display: inline-block;
+                align-items: center;
+            }
+            .twitchdownload-button:hover {
+                background-color: #772ce8;
+            }
+            .twitchdownload-button:active {
+                background-color: #5c16c5;
+            }
+            .button-text {
+                color: white;
+                margin: 0 6px 0 6px;
+                font-weight: bold;
+            }
+        `;
+
+        const styleElement = document.createElement('style');
+        styleElement.textContent = customStyles;
+        document.head.appendChild(styleElement);
+
+        // create the button and insert it
+        const divWrapper = document.createElement('div');
+        divWrapper.className = 'twitch-clip-downloader-userscript';
+        const button = document.createElement('button');
+        const buttonText = document.createElement('span');
+        buttonText.textContent = 'Download'; // button Text
+        button.className = 'twitchdownload-button';
+        // button click event listener
+        button.addEventListener('click', downloadClip);
+        buttonText.className = 'button-text';
+        button.appendChild(buttonText);
+        divWrapper.appendChild(button);
+        // insert the div wrapper before the last child
+        targetElement.insertBefore(divWrapper, targetElement.firstChild);
+    }
+
+    // function to remove the button
+    function removeButton() {
+        const existingButton = document.querySelector('.twitchdownload-button');
+        if (existingButton) {
+            existingButton.remove();
+        }
+    }
+
+    // function to download Twitch clip
     function downloadClip() {
         const videoElement = document.querySelector('video');
         if (videoElement) {
             const videoURL = videoElement.src;
-            // anchor download element
             const downloadLink = document.createElement('a');
             downloadLink.href = videoURL;
             downloadLink.click();
         }
     }
 
-    // button CSS style
-    const customStyles = `
-        .twitchdown-button {
-            background-color: #9147ff;
-            border: none;
-            border-radius: 0.4rem;
-            margin-right: 6px;
-            padding: 5px 2px 5px 2px;
-            cursor: pointer;
-            width: auto;
-            height: 30px;
-            display: inline-block;
-            align-items: center;
-        }
-        .twitchdown-button:hover {
-            background-color: #772ce8;
-        }
-        .twitchdown-button:active {
-            background-color: #5c16c5;
-        }
-        .button-text {
-            color: white;
-            margin: 0 6px 0 6px;
-            font-weight: bold;
-        }
-    `;
+    // MutationObserver to watch for DOM changes
+    const observer = new MutationObserver(createButton);
+    const observerOptions = {
+        childList: true,
+        subtree: true,
+    };
+    observer.observe(document.body, observerOptions);
 
-    const styleElement = document.createElement('style');
-    styleElement.textContent = customStyles;
-    document.head.appendChild(styleElement);
-
-    // create elements
-    function createButton() {
-        const divWrapper = document.createElement('div');
-        divWrapper.classList.add('twitch-clip-downloader-userscript');
-        const button = document.createElement('button');
-        const buttonText = document.createElement('span');
-        buttonText.textContent = 'Download Clip'; // button Text
-        button.className = 'twitchdown-button';
-        // button click event listener
-        button.addEventListener('click', downloadClip);
-        // append the button text to the button
-        buttonText.className = 'button-text';
-        button.appendChild(buttonText);
-        // append the button to the divwrapper
-        divWrapper.appendChild(button);
-        // find the element with class
-        const targetClass = '[class*="Layout-sc-1xcs6mc-0"][class*="player-controls__right-control-group"]';
-        const targetElement = document.querySelector(targetClass);
-        if (targetElement) {
-            targetElement.insertBefore(divWrapper, targetElement.firstChild);
-        }
-    }
-
-    // wait for the page load, then create the button
-    window.addEventListener('load', createButton);
+    createButton();
 })();
